a7d681b353163a5bb7fa9a73803777dbcacf8495
Jatin-dev12 <jatin@iosandweb.net>
2024-02-05 17:22:32 +0530

    Design is Done

--- js/src/App.js
+++ js/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './pages/Header';
 import Main from './pages/Main';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Chek from './pages/chek'
 
 function App() {
   return (
    <>
    <Header />
    <Main />
+   
    </>
   );
 }
--- js/src/pages/Main.js
+++ js/src/pages/Main.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../App.css';
+import { Row, Col ,Button, Container } from 'react-bootstrap';
+
+
+
 
 const Column = ({ children, width, shadow }) => {
   return (
@@ -9,29 +13,42 @@ const Column = ({ children, width, shadow }) => {
   );
 };
 
-const App = () => {
+const Main = () => {
   return (
     <>
     <div className="App">
       <Column  className="" width="75%"  shadow>
       <h2 className='yup'>Stake BUY Tokens
-</h2>
+</h2> 
+<Container>
+<Row>
+        <Col sm><h3>Participating Pool
+</h3> <p>Stake Your Buy Token and earn 8% APY
+</p>  </Col>
+        <Col sm></Col>
+        <Col sm><Button size="lg" className='chk'>Stake Now</Button></Col>
+      </Row>
+    </Container>
       
       </Column>
+      
            <Column width="25%">
         <h3 className='b'>Balance</h3>
-        <p>Staked</p>
-        <p>Rewards</p>
-        <p>Total</p>
-        <p>Total Value</p>
+        <p>Staked <span className='ss'>0</span></p>
+        <p>Rewards<span className='ss'>0</span></p>
+        <p>Total<span className='ss'></span></p>
+        <p>Total Value<span className='ss'>$0</span></p>
       </Column>
     </div>
-   <div>
-    <Column className="ll" width="75%"></Column>
+    <div>
+      <Col><h3 className='stake'>My Stakes</h3></Col>
     </div>
+
+    
+    
   
     </>
   );
 };
 
-export default App;
+export default Main;
